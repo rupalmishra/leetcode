c0bba7a4bc301686170033e73933dea710a75274
rupalmishra <rupalmishra.2003@gmail.com>
2024-08-07 17:39:20 +0530

    Initialize

---
+++ ToLowerCase.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+public class ToLowerCase{
+	
+	public static String convertToLowercase(String s) {
+        // Use the toLowerCase method to convert the string to lowercase
+        return s.toLowerCase();
+    }
+
+
+public static void main(String[] args) {
+    // Example string
+    String s = "Hello World! THIS is Java.";
+
+    // Convert to lowercase
+    String result = convertToLowercase(s);
+
+    // Print the resulting string
+    System.out.println(result);
+}
+}
+	
