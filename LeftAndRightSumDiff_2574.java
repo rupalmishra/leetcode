a3d516135fc1081aa0a0df30de7fb52bc96a82c7
rupalmishra <rupalmishra.2003@gmail.com>
2024-08-09 20:50:09 +0530

    LeftAndRightSumDiff_2574

---
+++ LeftAndRightSumDiff_2574.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] LeftAndRightSumDiff_2574(int[] nums) {
+        int n=nums.length;
+        int[] leftSum=new int[n];
+        for(int i=1; i<n;i++){
+            leftSum[i] = leftSum[i-1] + nums[i-1];
+        }
+        int[] rightSum=new int[n];
+        for(int i=n-2; i>=0;i--){
+            rightSum[i] = rightSum[i+1] + nums[i+1];
+        }
+        int[] ans=new int[n];
+        for(int i=0; i<n;i++){
+            ans[i]=Math.abs(leftSum[i]-rightSum[i]);
+        }
+        return ans;
+    }
+}
