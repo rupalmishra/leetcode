cdc31b30f8999becb23ebdac938d84beeabe52b4
rupalmishra <rupalmishra.2003@gmail.com>
2024-08-14 23:56:07 +0530

    FindWordsContainingChar_2942

---
+++ FindWordsContainingChar_2942.java
@@ -0,0 +1,12 @@
+    class FindWordsContainingChar_2942 {
+        public List<Integer> findWordsContaining(String[] words, char x) {
+            List<Integer> indices = new ArrayList<>();
+            for (int i = 0; i < words.length; i++) {
+                if (words[i].indexOf(x) != -1) {
+                    indices.add(i);
+                }
+            }
+            return indices;
+        }
+    }
+
