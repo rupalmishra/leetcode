96751bc7f67c6ae989be61dd8ce3209c1d5860b0
rupalmishra <rupalmishra.2003@gmail.com>
2024-08-15 23:51:42 +0530

    SortThePeople_2418

---
+++ SortThePeople_2418.java
@@ -0,0 +1,19 @@
+class SortThePeople_2418 {
+    public String[] sortPeople(String[] names, int[] heights) {
+        String[][] people = new String[heights.length][2];
+        
+        for (int i = 0; i < heights.length; i++) {
+            people[i][0] = Integer.toString(heights[i]);
+            people[i][1] = names[i];
+        }
+               
+        Arrays.sort(people, (a, b) -> Integer.parseInt(b[0]) - Integer.parseInt(a[0]));
+
+        String[] sortedNames = new String[names.length];
+        for (int i = 0; i < people.length; i++) {
+            sortedNames[i] = people[i][1];
+        }
+        
+        return sortedNames;
+    }
+}
