1650904ad5ff4884721fda5fca2e9eefdb822bb2
rupalmishra <rupalmishra.2003@gmail.com>
2024-08-21 23:32:49 +0530

    NoOfStrThatAppearAsSubstring_1967

---
+++ NoOfStrThatAppearAsSubstring_1967.java
@@ -0,0 +1,14 @@
+    class NoOfStrThatAppearAsSubstring_1967 {
+        public int numOfStrings(String[] patterns, String word) {
+            int count = 0;
+    
+            for (String pattern : patterns) {
+           
+                if (word.contains(pattern)) {
+                    count++;
+                }
+            }
+    
+            return count;
+        }
+    }
