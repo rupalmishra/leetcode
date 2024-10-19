df88147ba6306586f393810c10908c111ac22ea7
rupalmishra <rupalmishra.2003@gmail.com>
2024-10-19 22:28:17 +0530

    RichestCustomerWealth_1672

---
+++ RichestCustomerWealth_1672.java
@@ -0,0 +1,15 @@
+class RichestCustomerWealth_1672 {
+    public int maximumWealth(int[][] accounts) {
+        int maxWealth = 0;
+
+        for (int i = 0; i < accounts.length; i++) {
+            int customerWealth = 0;
+            for (int j = 0; j < accounts[i].length; j++) {
+                customerWealth += accounts[i][j];
+            }
+            maxWealth = Math.max(maxWealth, customerWealth);
+        }
+
+        return maxWealth;
+    }
+}
