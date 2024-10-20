d259a47f12f875be86adc91afa8c4c58876e3f0d
rupalmishra <rupalmishra.2003@gmail.com>
2024-10-20 22:13:46 +0530

    KidWithGreatestNoOfCandies_1431

---
+++ KidWithGreatestNoOfCandies_1431.java
@@ -0,0 +1,18 @@
+
+class KidWithGreatestNoOfCandies_1431 {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int maxCandies = 0;
+        for (int candy : candies) {
+            if (candy > maxCandies) {
+                maxCandies = candy;
+            }
+        }
+
+        List<Boolean> result = new ArrayList<>();
+        for (int candy : candies) {
+            result.add(candy + extraCandies >= maxCandies);
+        }
+
+        return result;
+    }
+}
