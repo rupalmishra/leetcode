fd6357f7b8e6cdd8e94d3200a533c4cbcb6d211a
rupalmishra <rupalmishra.2003@gmail.com>
2024-10-27 21:37:07 +0530

    FindIndicesOfStableMountains_3285

---
+++ FindIndicesOfStableMountains_3285.java
@@ -0,0 +1,11 @@
+class FindIndicesOfStableMountains_3285 {
+    public List<Integer> stableMountains(int[] height, int threshold) {
+        List<Integer> stableIndices = new ArrayList<>();
+        for (int i = 1; i < height.length; i++) {
+            if (height[i - 1] > threshold) {
+                stableIndices.add(i);
+            }
+        }
+        return stableIndices;
+    }
+}
