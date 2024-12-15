570c58c57fa73246f498d42748b00c3abcb70eef
rupalmishra <rupalmishra.2003@gmail.com>
2024-12-15 22:40:13 +0530

    ReverseStrings2_541

---
+++ ReverseStrings2_541.java
@@ -0,0 +1,19 @@
+public class ReverseStrings2_541 {
+    public String reverseStr(String s, int k) {
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i += 2 * k) {
+            reverse(chars, i, Math.min(i + k - 1, chars.length - 1));
+        }
+        return new String(chars);
+    }
+
+    private void reverse(char[] chars, int start, int end) {
+        while (start < end) {
+            char temp = chars[start];
+            chars[start] = chars[end];
+            chars[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
