222cb9531fb70ce7753bbe1f1dd8e819e4b0b6f6
rupalmishra <rupalmishra.2003@gmail.com>
2024-12-20 22:49:51 +0530

    MaxProdDifferenceBetweenTwoPairs_1913

---
+++ MaxProdDifferenceBetweenTwoPairs_1913.java
@@ -0,0 +1,7 @@
+class MaxProdDifferenceBetweenTwoPairs_1913 {
+    public int maxProductDifference(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        return (nums[n - 1] * nums[n - 2]) - (nums[0] * nums[1]);
+    }
+}
