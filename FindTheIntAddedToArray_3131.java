21679a5eaa99d3531223c1ee7ae5d986f7491f60
rupalmishra <rupalmishra.2003@gmail.com>
2024-12-22 23:44:45 +0530

    FindTheIntAddedToArray_3131

---
+++ FindTheIntAddedToArray_3131.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+class FindTheIntAddedToArray_3131 {
+    public int addedInteger(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        return nums2[0] - nums1[0];
+    }
+}
